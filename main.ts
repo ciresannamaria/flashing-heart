eb6f21f2bba461db651b43bd225fc18df56daa22
cires.annamaria <63457648+ciresannamaria@users.noreply.github.com>
2024-02-29 08:36:31 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
